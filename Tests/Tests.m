2a5eff69262c8d365c45006b48e22e0f4c5f1b75
Martin Kiss <martin.kiss@me.com>
2016-01-09 08:39:00 +0100

    Add empty tests

---
+++ Tests/Tests.m
@@ -0,0 +1,17 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Martin Kiss on 9.1.16.
+//  Copyright © 2016 Martin Kiss. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+@end

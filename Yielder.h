6ea1063c27b2bc863e5f1adc0f5fc2c56bc27c34
Martin Kiss <martin.kiss@me.com>
2016-01-09 08:35:31 +0100

    Initial empty project

---
+++ Yielder.h
@@ -0,0 +1,13 @@
+//
+//  Yielder.h
+//  Yielder
+//
+//  Created by Martin Kiss on 9.1.16.
+//  Copyright © 2016 Martin Kiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Yielder : NSObject
+
+@end
---
+++ Yielder.m
@@ -0,0 +1,13 @@
+//
+//  Yielder.m
+//  Yielder
+//
+//  Created by Martin Kiss on 9.1.16.
+//  Copyright © 2016 Martin Kiss. All rights reserved.
+//
+
+#import "Yielder.h"
+
+@implementation Yielder
+
+@end

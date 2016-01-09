f9ea214e3235bd4c0d64ebbe826aeaa6609f88db
Martin Kiss <martin.kiss@me.com>
2016-01-09 15:26:00 +0100

    Measure speed using tests

--- Tests/Tests.m
+++ Tests/Tests.m
@@ -43,4 +43,67 @@
     XCTAssertEqualObjects([objects componentsJoinedByString:@""], @"BDF");
 }
 
+- (id)heavyTask {
+    for (NSUInteger i = 0; i < 2000; i ++) {
+        [NSDateFormatter new];
+    }
+    return [NSDateFormatter new];
+}
+
+- (void)test_speedOfHeavyTask {
+    [self measureBlock:^{
+        for (NSUInteger i = 0; i < 20; i ++) {
+            [self heavyTask];
+        }
+    }];
+}
+
+- (void)produceObjects: (NSUInteger)count {
+    for (NSUInteger index = 0; index < count; index ++) {
+        yield [self heavyTask];
+    }
+}
+
+- (NSArray *)buildArray: (NSUInteger)count {
+    NSMutableArray *array = [NSMutableArray new];
+    for (NSUInteger index = 0; index < count; index ++) {
+        [array addObject:[self heavyTask]];
+    }
+    return array;
+}
+
+const NSUInteger count = 1000;
+
+- (void)test_speedOfYield_enumeration {
+    [self measureBlock:^{
+        for (id object in Yield(self, produceObjects:count)) {
+            [object self];
+        }
+    }];
+}
+
+- (void)test_speedOfYield_collection {
+    __block NSArray *x = nil;
+    [self measureBlock:^{
+        x = Yield(self, produceObjects:count).allObjects;
+    }];
+    [x makeObjectsPerformSelector:@selector(self)];
+}
+
+- (void)test_speedOfArray_enumeration {
+    [self measureBlock:^{
+        for (id object in [self buildArray:count]) {
+            [object self];
+        }
+    }];
+}
+
+- (void)test_speedOfArray_collection {
+    __block NSArray *x = nil;
+    [self measureBlock:^{
+        x = [self buildArray:count];
+    }];
+    [x makeObjectsPerformSelector:@selector(self)];
+}
+
 @end

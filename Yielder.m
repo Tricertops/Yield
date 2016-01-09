7699cf40074bf98a6df634b2cf224b57f693551e
Martin Kiss <martin.kiss@me.com>
2016-01-09 14:06:50 +0100

    Another round of refactoring
    • Implement -allObjects effectively.
    • Mark finish explicitly and it handle properly.
    • Clarify semaphore meaning by method names.
    • Skip nils at higher level.

--- Yielder.m
+++ Yielder.m
@@ -15,16 +15,19 @@
 @property (copy) void (^block)(id);
 
 @property id nextObject;
-@property BOOL isRunning;
+@property NSMutableArray *remainingObjects;
+@property BOOL isFinished;
+
 @property dispatch_queue_t queue;
-@property dispatch_semaphore_t producingSemaphore;
-@property dispatch_semaphore_t consumingSemaphore;
+@property dispatch_semaphore_t consumptionSemaphore;
+@property dispatch_semaphore_t productionSemaphore;
+@property dispatch_semaphore_t finishingSemaphore;
 
 @end
 
 @implementation Yielder
 
-#pragma mark Attaching
+#pragma mark Initializing
 
 + (void *)key {
     return (__bridge void *)Yielder.class;
@@ -36,37 +39,11 @@
         self->_target = target;
         self->_block = block;
         objc_setAssociatedObject(target, Yielder.key, self, OBJC_ASSOCIATION_RETAIN);
+        
+        [self start];
     }
     return self;
 }
-
-+ (void)setObject:(id)value forKeyedSubscript:(NSObject *)target {
-    //TODO: Support multiple (recursive) enumerations at once.
-    Yielder *yielder = objc_getAssociatedObject(target, Yielder.key);
-    [yielder setNextObject:value];
-}
-
-#pragma mark Next Object
-
-@synthesize nextObject = _nextObject;
-
-- (id)nextObject {
-    while (self->_nextObject == nil)
-        [self continueExecution];
-    
-    id next = self->_nextObject;
-    self->_nextObject = nil;
-    if (next == self)
-        next = nil; //! Yielding self means end.
-    return next;
-}
-
-- (void)setNextObject:(id)next {
-    self->_nextObject = next;
-    [self continueEnumeration];
-}
-
-#pragma mark Threading
 
 - (void)start {
     dispatch_queue_t queue = dispatch_queue_create(nil, DISPATCH_QUEUE_SERIAL);
@@ -74,28 +51,100 @@
     dispatch_set_target_queue(queue, parent);
     self->_queue = queue;
     
-    self->_producingSemaphore = dispatch_semaphore_create(0);
-    self->_consumingSemaphore = dispatch_semaphore_create(0);
+    self->_consumptionSemaphore = dispatch_semaphore_create(0);
+    self->_productionSemaphore = dispatch_semaphore_create(0);
+    self->_finishingSemaphore = dispatch_semaphore_create(0);
     
     dispatch_async(queue, ^{
+        [self waitForProductionRequest];
         self->_block(self->_target);
-        [self setNextObject:self]; //! Yielding self means end.
+        [self signalFinish];
+        [self allowConsumption];
     });
 }
 
-- (void)continueExecution {
-    if (self->_queue)
-        dispatch_semaphore_signal(self->_consumingSemaphore);
-    else
-        [self start];
-    //TODO: Maybe wait limited time?
-    dispatch_semaphore_wait(self->_producingSemaphore, DISPATCH_TIME_FOREVER);
+#pragma mark Next Object
+
++ (void)setObject:(id)value forKeyedSubscript:(NSObject *)target {
+    if (value == nil)
+        return; //! Skip yielded nils.
+    
+    //TODO: Support multiple (recursive) enumerations at once.
+    Yielder *yielder = objc_getAssociatedObject(target, Yielder.key);
+    [yielder setNextObject:value];
 }
 
-- (void)continueEnumeration {
-    dispatch_semaphore_signal(self->_producingSemaphore);
-    //TODO: Maybe wait limited time?
-    dispatch_semaphore_wait(self->_consumingSemaphore, DISPATCH_TIME_FOREVER);
+@synthesize nextObject = _nextObject;
+
+- (BOOL)hasNextObject {
+    return (self->_nextObject != nil || self->_isFinished);
+}
+
+- (id)nextObject {
+    while ( ! [self hasNextObject]) {
+        [self requestProduction];
+        [self waitForConsumption];
+    }
+    id next = self->_nextObject;
+    self->_nextObject = nil; //! Nullify for next call.
+    return next;
+}
+
+- (void)setNextObject:(id)next {
+    if (self->_remainingObjects) {
+        if (next)
+            [self->_remainingObjects addObject:next];
+    }
+    else {
+        self->_nextObject = next;
+        [self allowConsumption];
+        [self waitForProductionRequest];
+    }
+}
+
+- (NSArray *)allObjects {
+    NSMutableArray *remaining = [NSMutableArray new];
+    self->_remainingObjects = remaining;
+    [self requestProduction];
+    [self waitForFinish];
+    self->_remainingObjects = nil;
+    return remaining;
+}
+
+#pragma mark Semaphores
+
+- (void)waitForProductionRequest {
+    if (self->_isFinished)
+        return;
+    
+    dispatch_semaphore_wait(self->_productionSemaphore, DISPATCH_TIME_FOREVER);
+}
+
+- (void)requestProduction {
+    dispatch_semaphore_signal(self->_productionSemaphore);
+}
+
+- (void)waitForConsumption {
+    if (self->_isFinished)
+        return;
+    
+    dispatch_semaphore_wait(self->_consumptionSemaphore, DISPATCH_TIME_FOREVER);
+}
+
+- (void)allowConsumption {
+    dispatch_semaphore_signal(self->_consumptionSemaphore);
+}
+
+- (void)waitForFinish {
+    if (self->_isFinished)
+        return;
+    
+    dispatch_semaphore_wait(self->_finishingSemaphore, DISPATCH_TIME_FOREVER);
+}
+
+- (void)signalFinish {
+    self->_isFinished = YES;
+    dispatch_semaphore_signal(self->_finishingSemaphore);
 }
 
 @end
